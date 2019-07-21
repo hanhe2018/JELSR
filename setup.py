11de03acb22870b08603e4011bff2ea5eaccb567
hanhe2018 <35318307+hanhe2018@users.noreply.github.com>
2019-07-21 07:13:07 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JELSR",                                     
+    version="0.0.1",                                       
+    author="hanhe",                                      
+    author_email="hanhe@ucdavis.edu",                     
+    description="JELSR_Feature_Selection",                  
+    long_description=long_description,                      
+    long_description_content_type="text/markdown",          
+    url="https://github.com/hanhe2018/JELSR",            
+    packages=setuptools.find_packages(),               
+    classifiers=[                                         
+        "Programming Language :: Python :: 3",             
+        "License :: OSI Approved :: MIT License",           
+        "Operating System :: OS Independent",               
+    ],
+)
+
+
+# In[1]:
+
+
+import setuptools
+
